9c71e1eaa07164591fed9ffd6bad9a2dde2f36ff
Afnan Muhammad <otakutotipotent@gmail.com>
2026-02-26 17:41:12 +0500

    feat: add links to sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import '@/app/ui/global.css';
-import { lusitana } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
